54121a322f7cb3d4e75ea2aff9cfc0ce6ea9962e
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-10-29 15:38:37 +0400

    Update

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,8 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
@@ -20,6 +23,37 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String[] parts = signatureString.split("\\s+");
+
+        String accessModifier = null;
+        String returnType = null;
+        String methodName = parts[parts.length - 1];
+
+        List<MethodSignature.Argument> arguments = new ArrayList<>();
+
+        if (parts.length >= 2) {
+            returnType = parts[parts.length - 2];
+        }
+
+        if (parts.length >= 3) {
+            accessModifier = parts[0];
+        }
+
+        String argsString = signatureString.substring(signatureString.indexOf('(') + 1, signatureString.indexOf(')'));
+
+        String[] args = argsString.split(",\\s*");
+
+        for (String arg : args) {
+            String[] argParts = arg.split("\\s+");
+            if (argParts.length == 2) {
+                arguments.add(new MethodSignature.Argument(argParts[0], argParts[1]));
+            }
+        }
+
+        MethodSignature methodSignature = new MethodSignature(methodName, arguments);
+        methodSignature.setAccessModifier(accessModifier);
+        methodSignature.setReturnType(returnType);
+
+        return methodSignature;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,6 +14,36 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> substrings = new ArrayList<>();
+        int startIndex = 0;
+
+        while (startIndex < source.length()) {
+            int nextDelimiterIndex = getNextDelimiterIndex(source, startIndex, delimiters);
+
+            if (nextDelimiterIndex == -1) {
+                substrings.add(source.substring(startIndex));
+                break;
+            }
+
+            substrings.add(source.substring(startIndex, nextDelimiterIndex));
+            startIndex = nextDelimiterIndex + 1;
+        }
+
+        return substrings;
+    }
+
+    private int getNextDelimiterIndex(String source, int startIndex, Collection<String> delimiters) {
+        int minIndex = -1;
+
+        for (String delimiter : delimiters) {
+            int index = source.indexOf(delimiter, startIndex);
+
+            if (index != -1 && (minIndex == -1 || index < minIndex)) {
+                minIndex = index;
+            }
+        }
+
+        return minIndex;
+
     }
 }
